0c52f691eb0bc8be22b4aa70ea7b7a1fb548d595
Tejashwini-Ambati <tejashwinicjo@gmail.com>
2025-09-03 16:00:50 +0530

    variables.tf

---
+++ eks-cluster/variables.tf
@@ -0,0 +1,23 @@
+variable "region" {
+  default = "us-east-1"
+}
+
+variable "cluster_name" {
+  default = "devops-eks-cluster"
+}
+
+variable "node_instance_type" {
+  default = "t3.medium"
+}
+
+variable "desired_capacity" {
+  default = 2
+}
+
+variable "min_size" {
+  default = 1
+}
+
+variable "max_size" {
+  default = 3
+}
